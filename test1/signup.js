a86b1b1f3d30e26f980b8041809d91bf55f4d48a
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-10 00:31:48 +0900

    Update signup.js

--- test1/signup.js
+++ test1/signup.js
@@ -1,12 +1,12 @@
 // Firebaseの設定
 const firebaseConfig = {
-    apiKey: "AIzaSyBbwKVEZxyAR8huecODyDB-o9iOaXQ33Js",
-    authDomain: "test-2d53b.firebaseapp.com",
-    projectId: "test-2d53b",
-    storageBucket: "test-2d53b.appspot.com",
-    messagingSenderId: "589505704797",
-    appId: "1:589505704797:web:8cedccf9c716c9af136eb4",
-    measurementId: "G-39E2NFDDQ8"
+    apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
+    authDomain: "projectc-01.firebaseapp.com",
+    projectId: "projectc-01",
+    storageBucket: "projectc-01.appspot.com",
+    messagingSenderId: "282949999420",
+    appId: "1:282949999420:web:bdae3b7002d2c853571c39",
+    measurementId: "G-QDQJGHXYLP"
   };
 
 // Firebaseの初期化

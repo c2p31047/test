36458dff59bb55dc1f5d8a73f5ad6f17c670cc71
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-09 18:40:47 +0900

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,49 +1,16 @@
-const mysql = require('mysql');
-
-// MySQLデータベースに接続
-const db = mysql.createConnection({
-    host: '127.0.0.1',      // データベースのホスト名
-    user: 'root',   // データベースユーザー名
-    password: '', // データベースパスワード
-    database: 'test' // データベース名
-});
-
-// データベースへの接続を確立
-db.connect((err) => {
-    if (err) {
-        console.error('データベースに接続できませんでした:', err);
-        throw err;
-    }
-    console.log('データベースに接続しました');
-});
-
-// 新規登録フォームからのPOSTリクエストを処理
-app.post('/signup', (req, res) => {
-    const { username, email, password } = req.body;
-
-    // ユーザー情報をデータベースに挿入するSQLクエリ
-    const insertUserQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
-
-    // パスワードをハッシュ化する処理（例：bcryptを使用）
-    // パスワードハッシュ化ライブラリのインポート
-    const bcrypt = require('bcryptjs');
-    
-    // パスワードをハッシュ化
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
-        if (err) {
-            console.error('パスワードのハッシュ化中にエラーが発生しました:', err);
-            res.status(500).json({ message: 'ユーザーの登録に失敗しました' });
-        } else {
-            // パスワードをハッシュ化した値を使用してデータベースに挿入
-            db.query(insertUserQuery, [username, email, hashedPassword], (err, result) => {
-                if (err) {
-                    console.error('データベースへの挿入中にエラーが発生しました:', err);
-                    res.status(500).json({ message: 'ユーザーの登録に失敗しました' });
-                } else {
-                    console.log('新しいユーザーがデータベースに登録されました');
-                    res.status(201).json({ message: 'ユーザーが登録されました' });
-                }
-            });
-        }
+// 新しいユーザーを作成
+firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        // ユーザーデータをデータベースに保存
+        database.ref('users/' + user.uid).set({
+            username: username,
+            email: email,
+            // 他のユーザーデータ
+        });
+        res.status(201).json({ message: 'ユーザーが登録されました' });
+    })
+    .catch((error) => {
+        console.error('ユーザーの作成中にエラーが発生しました:', error);
+        res.status(500).json({ message: 'ユーザーの登録に失敗しました' });
     });
-});

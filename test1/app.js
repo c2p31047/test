b8cbf849cd86596e1d3fe8529558ae46888b47d3
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-09 22:14:10 +0900

    Update app.js

--- test1/app.js
+++ test1/app.js
@@ -64,29 +64,3 @@ googleLoginButton.addEventListener('click', () => {
         });
 });
 
-passwordResetForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const email = emailField.value;
-
-    // Firebase Authenticationを使用して、パスワードリセットメールを送信
-    firebase.auth().sendPasswordResetEmail(email)
-        .then(() => {
-            // パスワードリセットメールの送信に成功した場合の処理
-            resetMessage.textContent = 'パスワードリセットメールを送信しました。メールボックスを確認してください。';
-            resetMessage.style.color = 'green';
-        })
-        .catch((error) => {
-            // パスワードリセットメールの送信に失敗した場合の処理
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.error('パスワードリセットエラー:', errorCode, errorMessage);
-
-            // エラーメッセージを表示
-            resetMessage.textContent = 'パスワードリセットメールの送信に失敗しました。';
-            resetMessage.style.color = 'red';
-        });
-
-    // メール入力フィールドをクリア
-    emailField.value = '';
-});

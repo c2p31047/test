5719044231d5643f6697480a46ea1534a5491ff9
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-31 01:22:06 +0900

    Create test.js

---
+++ new1/test.js
@@ -0,0 +1,35 @@
+// Firebase Realtime Databaseへの参照を取得
+var database = firebase.database();
+
+// 予約ボタンがクリックされたときの処理
+document.getElementById('reservation-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var location = document.getElementById('location').value;
+    var datetime = document.getElementById('datetime').value;
+
+    // Firebase Realtime Database内で場所と日時が一致する予約情報をクエリ
+    var reservationsRef = database.ref('reservations');
+    reservationsRef.orderByChild('location').equalTo(location).once('value', function(snapshot) {
+        var isAvailable = true;
+        snapshot.forEach(function(childSnapshot) {
+            var reservation = childSnapshot.val();
+            if (reservation.datetime === datetime) {
+                isAvailable = false;
+            }
+        });
+
+        if (isAvailable) {
+            // 予約が可能な場合、Firebase Realtime Databaseに新しい予約を追加
+            var newReservationRef = reservationsRef.push();
+            newReservationRef.set({
+                location: location,
+                datetime: datetime,
+                user: firebase.auth().currentUser.uid // ログインユーザーのUID
+            });
+            alert('予約が完了しました！');
+        } else {
+            alert('指定した場所と日時は既に予約されています。');
+        }
+    });
+});

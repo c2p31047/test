3fdd313b94d9db4b2d1067502fc9df89d6ca48b3
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-31 00:42:36 +0900

    Update firebase.js

--- new/firebase.js
+++ new/firebase.js
@@ -5,13 +5,14 @@ import 'firebase/auth'; // auth モジュールを追加
 
 // Firebaseの設定
 const firebaseConfig = {
-    apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
-    authDomain: "projectc-01.firebaseapp.com",
-    projectId: "projectc-01",
-    storageBucket: "projectc-01.appspot.com",
-    messagingSenderId: "282949999420",
-    appId: "1:282949999420:web:bdae3b7002d2c853571c39",
-    measurementId: "G-QDQJGHXYLP"
+  apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
+  authDomain: "projectc-01.firebaseapp.com",
+  databaseURL: "https://projectc-01-default-rtdb.firebaseio.com",
+  projectId: "projectc-01",
+  storageBucket: "projectc-01.appspot.com",
+  messagingSenderId: "282949999420",
+  appId: "1:282949999420:web:bdae3b7002d2c853571c39",
+  measurementId: "G-QDQJGHXYLP"
 };
 
 // Firebaseの初期化

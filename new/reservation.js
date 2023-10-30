ff5158236b6a8bfc71337fe345cee2a1b9cf3dd0
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-31 00:50:32 +0900

    Update reservation.js

--- new/reservation.js
+++ new/reservation.js
@@ -1,6 +1,22 @@
 // reservation.js
-
 import { database } from './firebase.js';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTS7OuSwFOqUXNHgWLwKARmV3cOcLN-Ec",
+  authDomain: "projectc-01.firebaseapp.com",
+  databaseURL: "https://projectc-01-default-rtdb.firebaseio.com",
+  projectId: "projectc-01",
+  storageBucket: "projectc-01.appspot.com",
+  messagingSenderId: "282949999420",
+  appId: "1:282949999420:web:bdae3b7002d2c853571c39",
+  measurementId: "G-QDQJGHXYLP"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
 
 const addLocationForm = document.getElementById('add-location-form');
 const locationNameInput = document.getElementById('location-name');

77540e0e64d205147018a46be59cf97346412743
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2023-10-31 00:19:24 +0900

    Update add_location.js

--- new/add_location.js
+++ new/add_location.js
@@ -1,14 +1,24 @@
 // add_location.js
 
-// Firebaseの設定とデータベース参照
-import { database } from './firebase.js';
+import { database, auth } from './firebase.js';
 
-// フォーム要素と入力フィールドを取得
+// Firebaseの認証状態を監視
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // ログイン済みの場合は何もしない
+        console.log('ユーザーがログインしています:', user);
+    } else {
+        // 未ログインの場合はログインページにリダイレクト
+        console.log('ユーザーが未ログインです。ログインページにリダイレクトします。');
+        window.location.replace('login.html'); // ログインページのURLに置き換えてください
+    }
+});
+
+// フォームの送信イベントリスナーを追加
 const addLocationForm = document.getElementById('add-location-form');
 const locationNameInput = document.getElementById('location-name');
 const locationDescriptionInput = document.getElementById('location-description');
 
-// フォームの送信イベントリスナーを追加
 addLocationForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -19,7 +29,7 @@ addLocationForm.addEventListener('submit', function (event) {
     // ロケーションをデータベースに追加
     const locationsRef = database.ref('locations');
     const newLocationRef = locationsRef.push();
-    
+
     // ロケーション情報を設定
     newLocationRef.set({
         name: locationName,
